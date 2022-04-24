6bd114e569b4e6f741485ea7278647a272d2e447
Leo Westerburg Burr <lfc.westerburgburr@gmail.com>
2022-04-24 22:54:14 +0100

    Better acceleration and Wall jump bugs

---
+++ CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    PlayerController playerController;
+    public Vector3 offset;
+
+    void Start() 
+    {
+        GameObject player = GameObject.Find("Player");
+        playerController = player.GetComponent<PlayerController>();
+    }
+
+    void Update() {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset + new Vector3(playerController.xInput * 3, 0, 0), Time.deltaTime * 3f);
+    }
+}
--- PlayerController.cs
+++ PlayerController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
-    BoxCollider2D bc;   
+    BoxCollider2D bc; 
+
+    public float maxFallSpeed;  
 
     [Header("Run")]
-    float xInput = 0f;
+    [HideInInspector] public float xInput = 0f;
     public float maxRunSpeed;
     public float acceleration;
     public float deceleration;
@@ -29,10 +31,16 @@ public class PlayerMove : MonoBehaviour
     [Space]
     public Vector2 groundCheckOffset;
     public Vector2 groundCheckSize;
+    [Space]
     public float coyoteTime;
     float coyoteTimeCounter;
+    [Space]
     public float jumpBuffer;
     float jumpBufferCounter;
+    //[Space]
+    //public float wallJumpTime;
+    //float wallJumpTimeCounter;
+    [Space]
 
     [Header("Wall Movement")]
     [SerializeField] bool onWall;
@@ -55,45 +63,42 @@ public class PlayerMove : MonoBehaviour
 
     void Update() {
         Grab();
-        Jump();
         WallSlide();
+        Jump();
 
         // Takes input for running and returns a value from 1 (right) to -1 (left)
-        xInput = Convert.ToSingle(Input.GetAxisRaw("Horizontal"));
+        xInput = Input.GetAxis("Horizontal");
     }
 
     void FixedUpdate() {
-        if (Math.Abs(rb.velocity.x) < Math.Abs(xInput) * maxRunSpeed) {
+        //if (wallJumpTimeCounter > 0) {return;}
+        /*if (Math.Abs(rb.velocity.x) < Math.Abs(xInput) * maxRunSpeed) {
             // Increases the velocity by acceleration until the max velocity is reached
             rb.velocity += new Vector2((maxRunSpeed * xInput) / acceleration, 0) * Time.deltaTime;
         } else if ((Math.Abs(rb.velocity.x) > Math.Abs(xInput) * maxRunSpeed) && xInput == 0) {
             // Decreases the velocity by deceleration until velocity reaches 0 
-            rb.velocity -= new Vector2(rb.velocity.x / (onGround ? deceleration : deceleration * 3), 0) * Time.deltaTime;
+            rb.velocity -= new Vector2(rb.velocity.x / (onGround ? deceleration : deceleration * 4), 0) * Time.deltaTime;
         } else {
             // Applies a velocity scaled by maxRunSpeed to the player depending on the direction of the input
             rb.velocity = new Vector2(xInput * maxRunSpeed, rb.velocity.y);
-        }
-    }
+        }*/
+        rb.velocity = new Vector2(xInput * maxRunSpeed, rb.velocity.y);
+
+        if (maxFallSpeed < rb.velocity.y) {return;}
+        rb.velocity = new Vector2(rb.velocity.x, maxFallSpeed);
+    } 
 
     void Jump() {
         // Checks whether the player is on the ground and if it is, replenishes coyote time, but if not, it starts to tick it down
         coyoteTimeCounter = onGround ?  coyoteTime :  coyoteTimeCounter - Time.deltaTime;
         // When player jumps, jump buffer counter is reset, if not, ticks it down slowly
         jumpBufferCounter = Input.GetButtonDown("Jump") ? jumpBuffer : jumpBufferCounter - Time.deltaTime;
+        //wallJumpTimeCounter = rightFootOnWall || leftFootOnWall ? wallJumpTime : wallJumpTimeCounter - Time.deltaTime;
         // Draws a box to check whether the player is standing on objects on the ground layer
         onGround = Physics2D.OverlapBox((Vector2)transform.position + groundCheckOffset, groundCheckSize, 0f, groundLayer);
         // Draws a box to check whether the player is touching a wall 
         rightFootOnWall = (Physics2D.OverlapBox((Vector2)transform.position + wallJumpCheckOffset, wallCheckSize, 0f, groundLayer));
         leftFootOnWall = (Physics2D.OverlapBox((Vector2)transform.position + new Vector2(-wallJumpCheckOffset.x, wallJumpCheckOffset.y), wallCheckSize, 0f, groundLayer));
-        // Checks whether the player has recently left a platform (coyote time) or
-        // ... has pressed the jump button just before they land (jump buffer)
-        // If so, sets the y velocity to jumpHeight
-        if (jumpBufferCounter > 0 && coyoteTimeCounter > 0) {
-            rb.velocity = new Vector2 (rb.velocity.x, jumpHeight);
-            jumpBufferCounter = 0;
-        } else if ((rightFootOnWall || leftFootOnWall) && Input.GetButtonDown("Jump")) {
-            rb.velocity = new Vector2 (maxRunSpeed * (rightFootOnWall ? -1 : 1), jumpHeight);
-        }
         
         // Increases gravity of player when falling down or when the jump button is let go mid-jump
         if (rb.velocity.y < 0 ) {
@@ -101,6 +106,17 @@ public class PlayerMove : MonoBehaviour
         } else if ((rb.velocity.y > 0) && (!Input.GetButton("Jump"))) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpHeight - 1) * Time.deltaTime;
         }
+
+        // Checks whether the player has recently left a platform (coyote time) or
+        // ... has pressed the jump button just before they land (jump buffer)
+        // If so, sets the y velocity to jumpHeight (jumps)
+        if (jumpBufferCounter > 0 && coyoteTimeCounter > 0) {
+            rb.velocity = new Vector2 (rb.velocity.x, jumpHeight);
+            jumpBufferCounter = 0;
+        } else if ((rightFootOnWall || leftFootOnWall) && Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector2 (maxRunSpeed * (rightFootOnWall ? -1 : 1), jumpHeight);
+        }    
+        Debug.Log(rb.velocity);
     }
 
     void Grab() {
